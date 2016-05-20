a5241eca95242917202266ee06e8167e14f3a744
Henrik Feldt <henrik@haf.se>
2016-05-20 17:16:15 +0200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require("path"),
     webpack = require("webpack"),
+    UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin'),
     loaders = require('./webpack-loaders.js');
 
 var createConfig = function(version) {
@@ -20,7 +21,33 @@ var createConfig = function(version) {
     },
     resolve: {
       extensions: ['', '.js']
-    }
+    },
+    plugins: [
+      new UglifyJsPlugin({
+        exclude: [ './node_modules/**/*' ],
+        compress: {
+          sequences     : false,  // join consecutive statemets with the “comma operator”
+          properties    : false,  // optimize property access: a["foo"] → a.foo
+          dead_code     : false,  // discard unreachable code
+          drop_debugger : false,  // discard “debugger” statements
+          unsafe        : false, // some unsafe optimizations (see below)
+          conditionals  : false,  // optimize if-s and conditional expressions
+          comparisons   : false,  // optimize comparisons
+          evaluate      : false,  // evaluate constant expressions
+          booleans      : false,  // optimize boolean expressions
+          loops         : false,  // optimize loops
+          unused        : false,  // drop unused variables/functions
+          hoist_funs    : false,  // hoist function declarations
+          hoist_vars    : false, // hoist variable declarations
+          if_return     : false,  // optimize if-s followed by return/continue
+          join_vars     : false,  // join var declarations
+          cascade       : false,  // try to cascade `right` into `left` in sequences
+          side_effects  : false,  // drop side-effect-free statements
+          global_defs   : {},
+          keep_fnames: true
+        }
+      })
+    ]
   }
 };
 
