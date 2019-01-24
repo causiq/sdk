a934bd46651433bb870af2518b8f27ed1f86e88f
björn <bjorn.lundkvist@netlight.com>
2019-01-24 16:02:16 +0100

    Work in progress updating logary project

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,14 @@
-var path = require("path"),
-    webpack = require("webpack"),
-    loaders = require('./webpack-loaders.js');
+var path = require("path")
 
-var createConfig = function(version) {
-  return {
-    cache: true,
-    devtool: 'source-map',
-    entry: {
-      'logary': path.join(__dirname, 'src', 'index.js')
-    },
-    output: {
-      path: path.join(__dirname, "dist"),
-      filename: 'logary.js',
-      libraryTarget: "umd",
-      library: "logary"
-    },
-    module: {
-      loaders: loaders
-    },
-    resolve: {
-      extensions: ['', '.js']
-    }
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    'logary': path.join(__dirname, 'src', 'index.js')
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: 'logary.js',
+    libraryTarget: 'umd',
+    library: "logary"
   }
-};
-
-module.exports = createConfig('2.0.2');
+}
