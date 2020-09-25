46d5d95690c2b1e0cc17476bf6591d2dba5a87e0
Henrik Feldt <henrik@haf.se>
2020-09-25 17:54:49 +0200

    A minified version that works

--- packages/logary-browser/main.ts
+++ packages/logary-browser/main.ts
@@ -1,41 +1,62 @@
-import Logary, { RuttaTarget, getLogary, LogLevel, Config } from 'logary'
+import Logary, { RuttaTarget, getLogary, LogLevel, Config, EventMessage } from 'logary'
 import { ValueOf } from "logary/src"
 import browser from '@logary/plugin-browser'
 import pkg from './package.json'
 
-function getConfig(init: [keyof Config, ValueOf<Config>][]): Config {
-  let appId: string = 'unset',
-    serviceName = window.location.hostname
+type Res = {
+  config: Config;
+  jsExec: EventMessage
+}
+
+function getConfig(init: [keyof Config | string, ValueOf<Config>][]): Res {
+  let appId: string = 'unset', serviceName = window.location.hostname, jsExec = null
   const libraryVersion = pkg.version
 
   for (let i = 0; i < init.length; i++) {
     switch (init[i][0]) {
-      case 'appId':
-        // @ts-ignore
-        appId = init[i][1] || appId
+      case 'appId': {
+        appId = init[i][1] as string || appId
         break
-      case 'serviceName':
-        // @ts-ignore
-        serviceName = init[i][1] || serviceName
+      }
+
+      case 'serviceName': {
+        serviceName = init[i][1] as string || serviceName
         break
+      }
+
+      case 'js': {
+        const ts = BigInt('1000000') * BigInt(init[i][1])
+        jsExec = new EventMessage('JS exec on load', null, null, LogLevel.info, ts)
+        break
+      }
     }
   }
 
   return {
-    serviceName,
-    libraryVersion,
-    appId,
-    minLevel: LogLevel.verbose,
-    targets: [ new RuttaTarget({ endpoint: 'https://i.logary.tech' }) ],
+    config: {
+      serviceName,
+      libraryVersion,
+      appId,
+      minLevel: LogLevel.verbose,
+      targets: [ new RuttaTarget({ endpoint: 'https://i.logary.tech' }) ],
+    },
+    jsExec: jsExec || new EventMessage('JS exec on load')
   }
 }
 
-export function initLogary(): Logary {
+function initLogary(): Logary {
   let instance: Logary
+
+  // @ts-ignore
+  console.log('init logary called, window.logary', logary, window.logary)
+
+  // @ts-ignore
+  const { config, jsExec } = getConfig(logary?.i || [])
 
   // real instance exists, but we just loaded the browser script; reconfigure
   // @ts-ignore
   if (logary != null && 'reconfigure' in logary) {
+    console.log('Logary already instantiated')
     // @ts-ignore
     logary.reconfigure()
     // @ts-ignore
@@ -43,16 +64,20 @@ export function initLogary(): Logary {
   }
   // @ts-ignore
   else if (logary != null) {
-    // instance not existing, create it and configure it from the init value
+    // console.log('Logary stub exists')
+    // instance does not exist, create it and configure it from the init value
     // @ts-ignore
-    instance = getLogary(getConfig(logary.i))
+    instance = getLogary(config)
   } else {
     // instance not existing and we don't have a config for it either
     // @ts-ignore
-    instance = getLogary(getConfig())
+    instance = getLogary(config)
+    console.log('else case')
   }
 
   browser(instance)
+
+  instance.getLogger('Logary', 'browser').log(LogLevel.info, jsExec)
   
   // @ts-ignore
   // return window.logary = instance // with rollup
--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,12 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    path: path.resolve(__dirname, 'packages', 'logary-browser', 'dist'),
+    // path: path.resolve(__dirname, 'packages', 'logary-browser', 'dist'),
+    path: path.resolve(__dirname, 'examples', 'with-plain-html', 'js'),
     filename: 'logary.min.js',
     library: 'logary',
     libraryTarget: 'umd',
-  },
+  }
   // Maybe we can optimise this so that logary -> @OT/{api,core,tracing} and each plugin e.g. -> @OT/{web}
   // externals: [
   //   /^@opentelemetry\/.+$/,
