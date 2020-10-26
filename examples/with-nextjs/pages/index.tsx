15fdd0afcf28675edd438f40ff310e565ce10aa6
Olov Sikstrom <olov.sikstrom@gmail.com>
2020-10-26 12:21:49 +0100

    A bit different approach, we are now using esm output for logary and logary-plugin-browser to enable us to get three shaking for our bundle logaru.min.js since there is a problem when referencing esm from react packages.

--- examples/with-nextjs/pages/index.tsx
+++ examples/with-nextjs/pages/index.tsx
@@ -2,8 +2,8 @@ import { useCallback } from "react"
 import Link from 'next/link'
 import Layout from "../components/Layout"
 import logary from '../lib/logary'
-// import { useLogger } from '@logary/plugin-react'
-// import { withLogary } from "@logary/plugin-nextjs"
+import { useLogger } from '@logary/plugin-react'
+import { withLogary } from "@logary/plugin-nextjs"
 
 function Button({ children, ...rest }) {
   return (
@@ -14,23 +14,21 @@ function Button({ children, ...rest }) {
 }
 
 function IndexPage() {
-  const asd = logary.appId
+  const { event } = useLogger('IndexPage')
 
-  // const { event } = useLogger('IndexPage')
-
-  // const handlePurchase = useCallback(() =>
-  //   event('Product purchased', {
-  //     amount: 20, currency: 'EUR'
-  //   }, {
-  //     id: "57b9d",
-  //     name: "Kiosk T-Shirt",
-  //     price: "55.00",
-  //     brand: "Kiosk",
-  //     category: "T-Shirts",
-  //     variant: "red",
-  //     dimension1: "M",
-  //     quantity: 1
-  //   }), [])
+  const handlePurchase = useCallback(() =>
+    event('Product purchased', {
+      amount: 20, currency: 'EUR'
+    }, {
+      id: "57b9d",
+      name: "Kiosk T-Shirt",
+      price: "55.00",
+      brand: "Kiosk",
+      category: "T-Shirts",
+      variant: "red",
+      dimension1: "M",
+      quantity: 1
+    }), [])
 
   const crashingHandler = useCallback(() => {
     throw new Error("Something went wrong")
@@ -41,38 +39,38 @@ function IndexPage() {
       <h1>Example Logary app</h1>
       <fieldset>
         <legend>Buttons for users in an e-commerce site</legend>
-        {/* <Button onClick={handlePurchase}>
+        <Button onClick={handlePurchase}>
           Make purchase
-        </Button> */}
+        </Button>
         <Button onClick={crashingHandler}>
           Crashes
         </Button>
         <Button data-track='Make alt. purchase'>
           Make alt. purchase
         </Button>
-      </fieldset>
+        </fieldset>
 
-      <fieldset>
-        <legend>
-          An example form
-        </legend>
-        <form>
-          <label htmlFor='email'>
-            E-mail:
-          </label>
-          <input id='email' type='email' />
-        </form>
-      </fieldset>
+        <fieldset>
+          <legend>
+            An example form
+          </legend>
+          <form>
+            <label htmlFor='email'>
+              E-mail:
+            </label>
+            <input id='email' type='email' />
+          </form>
+        </fieldset>
 
-      <fieldset>
-        <legend>Navigation events</legend>
+        <fieldset>
+          <legend>Navigation events</legend>
 
-        <Link href='/contact'>
-          <a>Contact us</a>
-        </Link>
-      </fieldset>
+          <Link href='/contact'>
+            <a>Contact us</a>
+          </Link>
+        </fieldset>
     </Layout>
   )
 }
 
-export default IndexPage
+export default withLogary(IndexPage, { logary })
