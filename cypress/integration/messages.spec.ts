dc4c517512b85102472898344859d4dd97b371b6
Henrik Feldt <henrik@haf.se>
2020-10-27 18:03:26 +0100

    Workaround for infinite crash and burn from NextJS

--- cypress/integration/messages.spec.ts
+++ cypress/integration/messages.spec.ts
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 
 context('Network Requests', () => {
+  beforeEach(() => {
+    cy.visit('/?env=test')
+  })
   it('sends events as beacons', () => {
     cy.route2('/api/logary', "true").as("logcall")
 
--- examples/with-nextjs-app/lib/logary.ts
+++ examples/with-nextjs-app/lib/logary.ts
@@ -2,7 +2,10 @@ import { ConsoleTarget, RuttaTarget, getLogary, LogLevel } from 'logary'
 import nextjs from '@logary/plugin-nextjs'
 import react from '@logary/plugin-react'
 
-// console.log('NEXT_PUBLIC_LOGARY_ENDPOINT', process.env.NEXT_PUBLIC_LOGARY_ENDPOINT)
+// Why this? Because process.env.NEXT_PUBLIC_LOGARY_ENDPOINT is compiled in
+// and compiling with NODE_ENV=test causes an infinite crash loop in the browser.
+const pars = new URLSearchParams(typeof window !== 'undefined' ? window.location.search : '')
+const env = pars.get('env')
 
 const instance = getLogary({
   minLevel: LogLevel.debug,
@@ -10,7 +13,10 @@ const instance = getLogary({
   targets: [
     new ConsoleTarget(),
     new RuttaTarget({
-      endpoint: process.env.NEXT_PUBLIC_LOGARY_ENDPOINT || 'https://i.logary.tech'
+      endpoint:
+        process.env.NEXT_PUBLIC_LOGARY_ENDPOINT
+        || (env && env.includes('test') && '/api/logary')
+        || 'https://i.logary.tech'
     })
   ]
 })
@@ -21,6 +27,7 @@ if (typeof window !== "undefined") {
   const browser = require('@logary/plugin-browser').default
   browser(instance)
 }
+
 nextjs(instance)
 react(instance)
 
